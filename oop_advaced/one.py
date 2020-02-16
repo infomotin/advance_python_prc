880aa70e53a7dd2760d171b9e645c77cbb934a1a
infomotin <touch.u.bc1000@gmail.com>
2020-02-16 14:54:35 +0600

    full

--- oop_advaced/one.py
+++ oop_advaced/one.py
@@ -4,4 +4,29 @@ class Vector:
         self.y =y 
 
     def __repr__(self):
-        return "{} ({},{})".format(**locals())
+        return "{} ({},{})".format(self.__class__.__name__,self.x,self.y)
+        #return
+
+v = Vector(5,3)
+
+print(dir(v))
+
+print(v.__dict__)
+print(v.__dict__['x'])
+
+v.__dict__['x'] = 45
+
+print(v.__dict__['x'])
+
+#'x' in v.__dict__: give Flase 
+#adding object 
+
+v.__dict__['z'] = 15
+
+print(v.__dict__['z'])
+
+#find
+getattr(v,'z')
+#in operations 
+hasattr(v,'+')
+
